ee4c372341683087bfe381f1d3b484182d28d9d1
Fred Morrison <frederick.morrison@outlook.com>
2025-03-19 10:59:00 -0600

    switch from GitLab to GitHub

--- Driver.java
+++ Driver.java
@@ -5,7 +5,7 @@ public class Driver
     {
         String title = String.format("Java Color Chooser Demo using java version %s", getJavaVersion());
         System.out.println(title);
-        Dimension scaledSize = SwingScreenUtilities.getScaledSize(.4, 10);
+        Dimension scaledSize = SwingScreenUtilities.getScaledSize(.6, 10);
         // Schedule a job for the event-dispatching thread:
         // creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(
@@ -19,27 +19,5 @@ public class Driver
         return String.format("%s.%s.%s.%s", runTimeVersion.feature(), runTimeVersion.interim(), runTimeVersion.update(), runTimeVersion.patch());
     }
 
-    /**
-     * Gets a rectangle that is scaled to a percentage of available device screen size,
-     * rounded to the specified multiple.
-     *
-     * @param pct - the percentage (> 0 and < 1.0) of available device screen size to use.
-     * @param multipleOf - value to round up the scaled size to be a multiple of.
-     * @return - a Dimension object that holds the scaled width and height.
-     */
-    private static Dimension getScaledSize(double pct, int multipleOf)
-    {
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        System.out.println(screenSize);
-        // sanity check for unreasonable percentage values
-        if (pct < 0.1 || pct > 1)
-            return screenSize;
-        System.out.format("Screen width=%d, height=%d%n", screenSize.width, screenSize.height);
-        final int frameHeight = (int) (screenSize.height * pct) / multipleOf * multipleOf;
-        final int frameWidth = (int) (screenSize.width * pct) / multipleOf * multipleOf;
-        Dimension frameSize = new Dimension(frameWidth, frameHeight);
-        System.out.format("scaled frame: width=%d, height=%d%n", frameWidth, frameHeight);
-        return frameSize;
-    }
 
 }
